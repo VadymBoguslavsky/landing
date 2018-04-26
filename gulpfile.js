3f01e7f496597f0ba9d47995ae37f727d1c74ca1
VadymBoguslavsky <axixe92@gmail.com>
2018-04-26 22:29:51 +0300

    add sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,15 @@ var gulp = require('gulp');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
-
+var sass = require('gulp-sass');
 gulp.task('minifycss', function () {
     gulp.src('style.css')
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('dist'));
+});
+gulp.task('sass', function () {
+  return gulp.src('style/styles.sass')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./style/'));
 });
